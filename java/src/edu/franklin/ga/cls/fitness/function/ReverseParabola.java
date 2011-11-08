4ca2c3a0a5f945872dbd934d94431900575812c7
Christopher Simons <csimons@users.noreply.github.com>
2011-11-08 05:33:21 +0000

    Created ReverseParabola function and corresponding test class.
    git-svn-id: http://simons.unfuddle.com/svn/simons_is@11 65754e11-650e-49f0-a06d-364ce528dc37

---
+++ java/src/edu/franklin/ga/cls/fitness/function/ReverseParabola.java
@@ -0,0 +1,17 @@
+package edu.franklin.ga.cls.fitness.function;
+
+import java.util.List;
+
+public class ReverseParabola implements Function
+{
+    public double f(List<Double> inputs)
+    {
+        if (inputs.size() != 1)
+            throw new IllegalArgumentException(
+                    "Expecting  1 input variable.");
+
+        double x = inputs.get(0);
+
+        return 100 - (x * x);
+    }
+}
