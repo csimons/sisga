5de6a964cb7a06d6ac6ab205ce7e6f089ad114f9
Chris Simons <csimons@users.noreply.github.com>
2011-12-12 16:59:51 -0500

    Added Schwefel function.

---
+++ src/cls/fitness/test/SchwefelTest.java
@@ -0,0 +1,40 @@
+package cls.fitness.test;
+
+import cls.fitness.Schwefel;
+import java.util.LinkedList;
+import java.util.List;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SchwefelTest
+{
+    @Test
+    public void expectedValues()
+    {
+        assertEquals(0, new Schwefel().f(wrap(0)), 0.0001);
+
+        /*
+         * TODO: Add more tests.
+         */
+    }
+
+    private List<Double> wrap(double x)
+    {
+        List<Double> variables = new LinkedList<Double>();
+
+        variables.add(x);
+
+        return variables;
+    }
+
+    private List<Double> wrap(double x, double y)
+    {
+        List<Double> variables = new LinkedList<Double>();
+
+        variables.add(x);
+        variables.add(y);
+
+        return variables;
+    }
+}
