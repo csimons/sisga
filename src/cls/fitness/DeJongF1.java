ff816ddc0bf9a2ba74dec258360bee3e7b88d714
Chris Simons <csimons@users.noreply.github.com>
2011-12-12 15:32:58 -0500

    Added reference comment to De Jong functions.

--- src/cls/fitness/DeJongF1.java
+++ src/cls/fitness/DeJongF1.java
@@ -2,6 +2,13 @@ package cls.fitness;
 
 import java.util.List;
 
+/*
+ * See Kenneth De Jong's doctoral thesis for details.
+ *
+ * Available at the time of this writing at:
+ *
+ * http://cs.gmu.edu/~eclab/kdj_thesis.html
+ */
 public class DeJongF1 implements Function
 {
     public double f(List<Double> inputs)
