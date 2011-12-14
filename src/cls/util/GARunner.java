f695cda0ad08c13d78ed98f78fd31d405d06434a
Christopher Simons <csimons@users.noreply.github.com>
2011-12-14 11:28:45 -0500

    Now writing execution summaries to data files.

--- src/cls/util/GARunner.java
+++ src/cls/util/GARunner.java
@@ -35,7 +35,7 @@ public class GARunner
         ga.init();
 
         if (verbose)
-            printBeginInfo(config);
+            System.out.println("\n" + getBeginInfo(config) + "\n");
 
         GAResultSet results = ga.run
         (
@@ -49,12 +49,13 @@ public class GARunner
         );
 
         if (verbose)
-            printEndInfo(results);
+            System.out.println(getEndInfo(results) + "\n");
 
         try
         {
             results.writeToGnuPlotDataFile(String.format(
-                "%s-%s.dat", config.getConfigName(), getDateString()));
+                "%s-%s.dat", config.getConfigName(), getDateString()),
+                getBeginInfo(config), getEndInfo(results));
         }
         catch (Exception e)
         {
@@ -90,18 +91,18 @@ public class GARunner
         System.exit(1);
     }
 
-    private static void printBeginInfo(Configuration config)
+    private static String getBeginInfo(Configuration config)
     {
-        System.out.println(String.format(""
-            + "\nRunning %s for %d generations "
-                + "or until fitness %f is reached.\n\n"
-            + "    Population size:       %d\n"
-            + "    Chromosome size:       %d\n"
-            + "    Crossover probability: %f\n"
-            + "    Mutation probability:  %f\n"
-            + "    Terminal generation:   %d\n"
-            + "    Terminal fitness:      %f\n"
-            + "    FPP (decimal places):  %d\n",
+        return String.format(""
+            + "# Running %s for %d generations,\n"
+            + "# or until fitness %f is reached.\n#\n"
+            + "#     Population size:       %d\n"
+            + "#     Chromosome size:       %d\n"
+            + "#     Crossover probability: %f\n"
+            + "#     Mutation probability:  %f\n"
+            + "#     Terminal generation:   %d\n"
+            + "#     Terminal fitness:      %f\n"
+            + "#     FPP (decimal places):  %d",
             config.getGA(),
             config.getTermGeneration(),
             config.getTermFitness(),
@@ -111,27 +112,27 @@ public class GARunner
             config.getPM(),
             config.getTermGeneration(),
             config.getTermFitness(),
-            config.getFPP()));
+            config.getFPP());
     }
 
-    private static void printEndInfo(GAResultSet results)
+    private static String getEndInfo(GAResultSet results)
     {
-        System.out.println(String.format(""
-            + "Results:\n\n"
-            + "    Beginning best fitness:    %f\n"
-            + "    Beginning worst fitness:   %f\n"
-            + "    Beginning average fitness: %f\n\n"
-            + "    Terminal best fitness:     %f\n"
-            + "    Terminal worst fitness:    %f\n"
-            + "    Terminal average fitness:  %f\n"
-            + "    Terminal generation:       %d\n\n",
+        return String.format(""
+            + "# Results:\n#\n"
+            + "#     Beginning best fitness:    %f\n"
+            + "#     Beginning worst fitness:   %f\n"
+            + "#     Beginning average fitness: %f\n#\n"
+            + "#     Terminal best fitness:     %f\n"
+            + "#     Terminal worst fitness:    %f\n"
+            + "#     Terminal average fitness:  %f\n"
+            + "#     Terminal generation:       %d",
             results.bestFitnesses.get(0),
             results.worstFitnesses.get(0),
             results.avgFitnesses.get(0),
             results.bestFitnesses.get(results.bestFitnesses.size() - 1),
             results.worstFitnesses.get(results.worstFitnesses.size() - 1),
             results.avgFitnesses.get(results.avgFitnesses.size() - 1),
-            results.avgFitnesses.size() - 1));
+            results.avgFitnesses.size() - 1);
     }
 
     private static String getDateString()
