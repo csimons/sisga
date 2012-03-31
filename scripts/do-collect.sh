4459df9b8caa13fcd4128b0dd1342ba72c81012e
Chris Simons <csimons@users.noreply.github.com>
2012-03-31 00:44:35 -0400

    Added do-collect.sh script.

---
+++ scripts/do-collect.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Run all configurations specified number of times.
+
+if [ ! $# -eq 1 ]
+then
+    echo "usage: <script> <run-count>"
+    exit 1
+fi
+
+if [ ! -f dist/sisga.jar ]
+then
+    echo "You must build the project first."
+    exit 1
+fi
+
+n=$1
+
+for configFile in config/*.properties
+do
+    rawName=${configFile%\.*};
+
+    echo -n "Collecting for configuration $rawName ... "
+
+    i=0
+    while [ $i -lt $n ]
+    do
+        java -jar dist/sisga.jar $rawName
+        i=$[$i+1]
+    done
+
+    echo "done."
+done
